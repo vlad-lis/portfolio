5837875b6e303e04bb796f090b0f9d034e035c0a
vladislav lis <lis.vladislav@gmail.com>
2023-07-17 07:08:01 +0400

    Removed font preloader from html head; Added webfontloader into App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import WebFont from 'webfontloader';
 import Header from '../Header/Header';
 import Projects from '../Projects/Projects';
 import About from '../About/About';
 import Footer from '../Footer/Footer';
 
 function App() {
+  useEffect(() => {
+    WebFont.load({
+      custom: {
+        families: ['Ubuntu', 'Ubuntu-light'],
+        urls: ['/vendor/fonts.css'],
+      },
+    });
+  }, []);
+
   return (
     <div className="root">
       <Routes>
